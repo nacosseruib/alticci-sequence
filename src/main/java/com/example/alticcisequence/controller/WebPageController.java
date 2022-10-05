b3ebfb7827b1c51eb3804ffda05f30d751c7c7f5
Samson Ajax <nacosseruib@gmail.com>
2022-10-05 09:43:14 +0100

    Alticci Sequence project

--- src/main/java/com/example/alticcisequence/controller/WebPageController.java
+++ src/main/java/com/example/alticcisequence/controller/WebPageController.java
@@ -3,6 +3,7 @@ package com.example.alticcisequence.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,17 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class WebPageController {
 
-    //@RequestMapping("/")
     @GetMapping({"/"})
     public ModelAndView homePage(Model model) {
-        AlticciSequenceController AlticciSequenceController = new AlticciSequenceController();
-        model.addAttribute("result", AlticciSequenceController.mapAlticci(6));
+        //        AlticciSequenceController AlticciSequenceController = new AlticciSequenceController();
+        //        model.addAttribute("result", AlticciSequenceController.mapAlticci(n));
         return new ModelAndView("index");
-
-//        AlticciSequenceController AlticciSequenceController = new AlticciSequenceController();
-//        ModelAndView model = new ModelAndView("/index");
-//        model.addObject("result", AlticciSequenceController.mapAlticci(6));
-//        return model;
 
     }
 
